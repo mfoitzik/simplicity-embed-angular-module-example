3d50a01ab96f30a193801aa03741287a7f73cc25
Mike Foitzik <mike@mifo.com>
2023-12-22 06:42:26 -0800

    Fixed attribute references

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SimplicityEmbed } from '@simplicitywebtools/simplicity-embed-angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simplicity-embed-angular-module-example';
+  seSetup = "http://localhost:5500/config.json";
+  private sembed: SimplicityEmbed | null = null;
+  openSimplicityEmbed() {
+    this.sembed?.open();
+  }
+  closeSimplicityEmbed() {
+    this.sembed?.close();
+  }
+  sendCommand() {
+    const sendObj = {
+      "action": "spin"
+    }
+    this.sembed?.sendMessage(sendObj);
+  }
+  changeConfig() {
+    this.seSetup = "http://localhost:5500/config2.json";
+  }
+  handleNotify(event: Event) {
+    const customEvent = event as CustomEvent<any>;
+    console.log("I received a notify event");
+    console.log(customEvent.detail);
+  }
+  ngOnInit() {
+    this.sembed = document.getElementById("simplicity") as any;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SimplicityEmbed} from '@simplicitywebtools/simplicity-embed-angular'
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SimplicityEmbed
   ],
   providers: [],
   bootstrap: [AppComponent]
